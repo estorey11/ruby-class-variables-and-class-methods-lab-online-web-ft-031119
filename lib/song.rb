9926acd249305daf1c533eb1d94fcc4b79d3ace1
Eric Storey <estorey@gmail.com>
2019-03-20 20:24:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,15 @@ class Song
     
   end
   
+  def self.genre_count
+    genre_count=Hash.new(0)
+    @@genres.each {|genre|
+      genre_count[genre]+=1
+    }
+    
+    genre_count
+    
+  end
   
 end
 
